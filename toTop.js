cf3c1dd5d98b7d87c00b667693e5475fa5c46229
Kierrhtml <123918731+Kierrhtml@users.noreply.github.com>
2023-01-30 11:49:07 +0800

    Add files via upload

---
+++ toTop.js
@@ -0,0 +1,26 @@
+
+let mybutton = document.getElementById("btn-back-to-top");
+let nav = document.getElementById("navigation")
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    mybutton.style.display = "block";
+   
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+}
+
